aea25d67ab053e290e4b7cc406c175c180e3dfc4
Hanse <hanse19@gmail.com>
2014-05-30 10:31:12 +0200

    Use clusters

--- index.js
+++ index.js
@@ -1,56 +1,73 @@
 
-var express = require('express');
-var bodyParser = require('body-parser');
-var stylus = require('stylus');
-var nib = require('nib');
-var app = module.exports = express();
+var cluster = require('cluster');
 
-/**
- * Config
- */
+if (cluster.isMaster) {
 
-app.set('port', process.env.PORT || 5000);
-app.set('view engine', 'jade');
-app.set('views', __dirname + '/views');
-app.use(bodyParser());
-app.locals.pretty = true;
+  var cpuCount = require('os').cpus().length;
+  for (var i = 0; i < cpuCount; ++i) {
+    cluster.fork();
+  }
 
-app.use(stylus.middleware({
-  src: __dirname + '/public',
-  dest: __dirname + '/public',
-  compress: app.get('env') !== 'development',
-  compile: function(str, path) {return stylus(str).set('filename', path).use(nib());}
-}));
+  cluster.on('exit', function(worker) {
+    console.log('Worker %d died', worker.id);
+    cluster.fork();
+  });
 
-/**
- * Development Config
- */
+} else {
 
-if ('development' === app.get('env')) {
-  app.use(express.static(__dirname + '/public'));
+  var express = require('express');
+  var bodyParser = require('body-parser');
+  var stylus = require('stylus');
+  var nib = require('nib');
+  var app = module.exports = express();
+
+  /**
+   * Config
+   */
+
+  app.set('port', process.env.PORT || 5000);
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/views');
+  app.use(bodyParser());
+  app.locals.pretty = true;
+
+  app.use(stylus.middleware({
+    src: __dirname + '/public',
+    dest: __dirname + '/public',
+    compress: app.get('env') !== 'development',
+    compile: function(str, path) {return stylus(str).set('filename', path).use(nib());}
+  }));
+
+  /**
+   * Development Config
+   */
+
+  if ('development' === app.get('env')) {
+    app.use(express.static(__dirname + '/public'));
+  }
+
+  /**
+   * Index Page
+   */
+
+  app.get('/', function(req, res) {
+    return res.render('index', {projects: require('./projects.json')});
+  });
+
+  /**
+   * 404
+   */
+
+  app.use(function(req, res, next) {
+    res.status(404);
+    return res.render('404', {back: '/'});
+  });
+
+  /**
+   * Listen
+   */
+
+  app.listen(app.get('port'), function() {
+    console.log('Worker %d: app listening on %d', cluster.worker.id, app.get('port'));
+  });
 }
-
-/**
- * Index Page
- */
-
-app.get('/', function(req, res) {
-  return res.render('index', {projects: require('./projects.json')});
-});
-
-/**
- * 404
- */
-
-app.use(function(req, res, next) {
-  res.status(404);
-  return res.render('404', {back: '/'});
-});
-
-/**
- * Listen
- */
-
-app.listen(app.get('port'), function() {
-  console.log('app listening on %d', app.get('port'));
-});
