1cfab99ee358ce291d7e30db5fa7e8d19257dcfe
Hans-Kristian Koren <hanse@koren.im>
2018-10-20 10:10:15 +0200

    update content

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import Link from 'gatsby-link';
-import { css } from 'glamor';
+import React from "react";
+import Link from "gatsby-link";
+import { css } from "glamor";
 
-import Header from '../components/Header';
-import Project from '../components/Project';
-import me from '../me.png';
-import { MOBILE, opacityIn } from '../styles';
+import Header from "../components/Header";
+import Project from "../components/Project";
+import me from "../me.png";
+import { MOBILE, opacityIn } from "../styles";
 
 const PROFILE_IMAGE_SIZE = 160;
 
@@ -14,20 +14,17 @@ const IndexPage = () => (
     <Header />
     <div
       css={{
-        display: 'flex',
-        alignItems: 'center',
+        display: "flex",
+        alignItems: "center",
         [MOBILE]: {
-          flexDirection: 'column-reverse'
+          flexDirection: "column-reverse"
         }
       }}
     >
       <p css={{ flex: 1 }}>
-        I'm a developer mainly working with financial software at{' '}
-        <a href="https://itera.no">Itera</a> and{' '}
-        <a href="https://cicero.no">Cicero</a> plus various side projects.
-        Currently I'm mostly doing typed JavaScript with Flow, React and React
-        Native development, but I also have some experience with Python, Ruby
-        (on Rails) and Java.
+        I'm a developer working at the consultancy firm{" "}
+        <a href="https://itera.no">Itera</a>. I'm working with web frontends,
+        mobile apps and backend systems using appropriate tools.
       </p>
       <img
         src={me}
@@ -43,7 +40,7 @@ const IndexPage = () => (
           animationDuration: `0.4s`,
           animationName: `${opacityIn}`,
           animationIterationCount: 1,
-          animationFillMode: 'both'
+          animationFillMode: "both"
         }}
       />
     </div>
@@ -54,7 +51,7 @@ const IndexPage = () => (
       index={0}
       name="Listfully"
       url="https://listfully.no/"
-      tags={['JavaScript', 'React Native', 'iOS', 'Android']}
+      tags={["JavaScript", "React Native", "iOS", "Android"]}
       description="Create and share your own gift lists. An app I created for a client for both iOS and Android."
       appStore="https://itunes.apple.com/us/app/listfully/id1252572755?mt=8"
       playStore="https://play.google.com/store/apps/details?id=com.listfully"
@@ -62,15 +59,15 @@ const IndexPage = () => (
     <Project
       index={1}
       name="Ninjagains"
-      tags={['JavaScript', 'React Native', 'iOS', 'Android']}
+      tags={["JavaScript", "React Native", "iOS", "Android"]}
       url="https://getninjagains.com"
-      description={'Track your strength workouts effortlessly.'}
+      description={"Track your strength workouts effortlessly."}
       appStore="https://itunes.apple.com/no/app/ninjagains/id1400435707"
     />
     <Project
       index={2}
       name="Fuuto"
-      tags={['JavaScript', 'React Native', 'iOS', 'Android']}
+      tags={["JavaScript", "React Native", "iOS", "Android"]}
       description="A social football app I built for a client a couple of years ago. It was available on both iOS and Android."
       url="https://itunes.apple.com/us/app/fuuto/id1108571181?mt=8"
       appStore="https://itunes.apple.com/us/app/fuuto/id1108571181?mt=8"
@@ -80,51 +77,52 @@ const IndexPage = () => (
       index={3}
       name="Fakturamaker.no"
       url="https://fakturamaker.no"
-      tags={['JavaScript', 'Node.js', 'Angular.js']}
+      tags={["JavaScript", "Node.js", "Angular.js"]}
       description="The tool I use to create invoices."
     />
     <Project
       index={4}
       name="Timetracker"
       url="https://timetracker.koren.im"
-      tags={['JavaScript', 'React']}
+      tags={["JavaScript", "React"]}
       description="A very beta-ish tool for tracking time spent on different tasks during the course of a day."
     />
     <Project
       index={5}
       name="react-calendar"
-      url="https://github.com/Hanse/react-calendar"
-      tags={['JavaScript', 'React', 'npm']}
+      url="https://github.com/hanse/react-calendar"
+      tags={["JavaScript", "React", "npm"]}
       description="A flexible React Component for rendering standard monthly calendars."
     />
     <Project
       index={6}
       name="Abakus.no"
       url="https://abakus.no"
-      tags={['JavaScript', 'React']}
+      tags={["JavaScript", "React"]}
       description="During my time at the Norwegian University of Science and Technology I helped build the new website for the fraternity Abakus. A rather big project written in React and Django."
     />
     <Project
       index={7}
       name="Running Pace Calculator"
       url="https://running.koren.im"
-      tags={['JavaScript', 'React', 'React Native Web']}
+      tags={["JavaScript", "React", "React Native Web"]}
       description="Calculate the required pace and speed to reach a running time-goal."
     />
     <Project
       index={8}
       name="Maconomy CLI"
       url="https://github.com/hanse/maconomy"
-      tags={['Node.js']}
+      tags={["Node.js"]}
       description="JavaScript library and command line interface for Deltek's Maconomy time reporting software. Made it because their UI is objectively useless and I have to use it every week."
     />
     <h2>
       <span>Contact</span>
     </h2>
-    I'm all over the internet, including{' '}
-    <a href="https://www.linkedin.com/in/hanskristiankoren/">LinkedIn</a>,{' '}
-    <a href="https://twitter.com/hanse">Twitter</a> and{' '}
-    <a href="https://github.com/hanse">GitHub</a>. You can also email me at{' '}
+    I'm all over the internet, including{" "}
+    <a href="https://www.linkedin.com/in/hanskristiankoren/">LinkedIn</a>,{" "}
+    <a href="https://twitter.com/hanse">Twitter</a>,{" "}
+    <a href="https://keybase.io/hanse">Keybase</a> and{" "}
+    <a href="https://github.com/hanse">GitHub</a>. You can also email me at{" "}
     <a href="mailto:hanse@koren.im">hanse@koren.im</a>.
   </div>
 );
